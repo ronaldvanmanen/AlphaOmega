1ccf30f0cc84076a7982464ecf6eca31dc2713c6
Ronald van Manen <rvanmanen@gmail.com>
2024-09-07 13:42:41 +0200

    Disable Spring banner.

--- src/main/java/com/ragnvaldr/alphaomega/Application.java
+++ src/main/java/com/ragnvaldr/alphaomega/Application.java
@@ -21,13 +21,16 @@
 package com.ragnvaldr.alphaomega;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.Banner.Mode;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		var application = new SpringApplication(Application.class);
+		application.setBannerMode(Mode.OFF);
+		application.run(args);
 	}
 
 }
