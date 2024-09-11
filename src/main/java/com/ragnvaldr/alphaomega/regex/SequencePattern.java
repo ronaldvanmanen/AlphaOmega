033956078b5c5359e24d8773df96b0ab9002350b
Ronald van Manen <rvanmanen@gmail.com>
2024-09-11 14:08:09 +0200

    Add a pattern that matches two or more patterns in sequence.

---
+++ src/main/java/com/ragnvaldr/alphaomega/regex/SequencePattern.java
@@ -0,0 +1,51 @@
+// Alpha Omega
+// 
+// Copyright (C) 2024 Ronald van Manen <rvanmanen@gmail.com>
+// 
+// This software is provided 'as-is', without any express or implied
+// warranty.  In no event will the authors be held liable for any damages
+// arising from the use of this software.
+// 
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+// 
+// 1. The origin of this software must not be misrepresented; you must not
+//    claim that you wrote the original software. If you use this software
+//    in a product, an acknowledgment in the product documentation would be
+//    appreciated but is not required.
+// 2. Altered source versions must be plainly marked as such, and must not be
+//    misrepresented as being the original software.
+// 3. This notice may not be removed or altered from any source distribution.
+
+package com.ragnvaldr.alphaomega.regex;
+
+import java.util.Collection;
+
+import com.ragnvaldr.alphaomega.Scanner;
+
+public final class SequencePattern extends Pattern {
+
+    private Pattern[] patterns;
+
+    public SequencePattern(Pattern... patterns) {
+        this.patterns = patterns;
+    }
+
+    public SequencePattern(Collection<Pattern> patterns) {
+        this.patterns = patterns.toArray(new Pattern[0]);
+    }
+
+    public boolean matches(Scanner scanner) {
+        var position = scanner.getPosition();
+
+        for (var pattern : patterns) {
+            if (!pattern.matches(scanner)) {
+                scanner.setPosition(position);
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
