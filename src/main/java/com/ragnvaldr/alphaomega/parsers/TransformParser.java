3d6607c3355a324b8fe5652baa641be26586e9af
Ronald van Manen <rvanmanen@gmail.com>
2024-09-11 14:18:07 +0200

    Add a parser that transforms the parse result of another parser on success.

---
+++ src/main/java/com/ragnvaldr/alphaomega/parsers/TransformParser.java
@@ -0,0 +1,32 @@
+package com.ragnvaldr.alphaomega.parsers;
+
+import java.util.function.Function;
+import java.util.function.Supplier;
+import com.ragnvaldr.alphaomega.Scanner;
+
+public final class TransformParser<TTarget, TSource> implements Parser<TTarget> {
+ 
+    private Parser<TSource> parser;
+
+    private Function<TSource, TTarget> transform;
+
+    public TransformParser(Parser<TSource> parser, Supplier<TTarget> transform)  {
+        this(parser, _ -> transform.get());
+    }
+
+    public TransformParser(Parser<TSource> parser, Function<TSource, TTarget> transform) {
+        this.parser = parser;
+        this.transform = transform;
+    }
+
+    public ParseResult<TTarget> parse(Scanner scanner) {
+        var parseResult = parser.parse(scanner);
+        if (parseResult.isSuccess()) {
+            return ParseResult.success(
+                transform.apply(parseResult.getValue())
+            );
+        }
+        return ParseResult.failure();
+    }
+    
+}
