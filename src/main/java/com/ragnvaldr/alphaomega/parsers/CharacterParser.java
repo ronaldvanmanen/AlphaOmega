220b2c349084d771cb0f8886ee299d6ae732707a
Ronald van Manen <rvanmanen@gmail.com>
2024-09-11 14:12:11 +0200

    Add parsers that parse single characters and strings.

---
+++ src/main/java/com/ragnvaldr/alphaomega/parsers/CharacterParser.java
@@ -0,0 +1,55 @@
+// Alpha Omega
+// 
+// Copyright (C) 2024 Ronald van Manen <rvanmanen@gmail.com>
+// 
+// This software is provided 'as-is', without any express or implied
+// warranty.  In no event will the authors be held liable for any damages
+// arising from the use of this software.
+// 
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+// 
+// 1. The origin of this software must not be misrepresented; you must not
+//    claim that you wrote the original software. If you use this software
+//    in a product, an acknowledgment in the product documentation would be
+//    appreciated but is not required.
+// 2. Altered source versions must be plainly marked as such, and must not be
+//    misrepresented as being the original software.
+// 3. This notice may not be removed or altered from any source distribution.
+
+package com.ragnvaldr.alphaomega.parsers;
+
+import java.util.function.Predicate;
+
+import com.ragnvaldr.alphaomega.Scanner;
+
+public final class CharacterParser implements Parser<Character> {
+    
+    private Predicate<Character> predicate;
+
+    public CharacterParser(char character) {
+        this(c -> c == character);
+    }
+
+    public CharacterParser(Predicate<Character> predicate) {
+        this.predicate = predicate;
+    }
+
+    public ParseResult<Character> parse(Scanner scanner) {
+        var position = scanner.getPosition();
+
+        var value = scanner.read();
+        if (value != -1) {
+            var character = (char)value;
+            if (predicate.test(character)) {
+                return ParseResult.success(character);
+            }
+        }
+
+        scanner.setPosition(position);
+
+        return ParseResult.failure();
+    }
+
+}
