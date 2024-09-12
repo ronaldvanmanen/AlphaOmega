224da30224957acde40fea53448606383c53639a
Ronald van Manen <rvanmanen@gmail.com>
2024-09-12 10:07:14 +0200

    Make Either null safe.

--- src/main/java/com/ragnvaldr/alphaomega/util/Either.java
+++ src/main/java/com/ragnvaldr/alphaomega/util/Either.java
@@ -20,43 +20,28 @@
 
 package com.ragnvaldr.alphaomega.util;
 
-import org.springframework.lang.Nullable;
+import java.util.NoSuchElementException;
 
-public final class Either<T, S> {
-
-    private @Nullable T left;
-    private @Nullable S right;
-
-    private Either(@Nullable T left, @Nullable S right) {
-        this.left = left;
-        this.right = right;
-    }
-
-    public boolean isLeft() {
-        return left != null;
-    }
-
-    public boolean isRight() {
-        return right != null;
-    }
+public abstract class Either<T, S> {
 
     public static <T, S> Either<T, S> left(T value) {
-        return new Either<>(value, null);
+        return new Left<T, S>(value);
     }
 
     public static <T, S> Either<T, S> right(S value) {
-        return new Either<>(null, value);
+        return new Right<T, S>(value);
     }
 
-    public @Nullable T getLeft() {
-        return left;
+    public abstract boolean isLeft();
+
+    public boolean isRight() {
+        return !isLeft();
     }
 
-    public @Nullable S getRight() {
-        return right;
-    }
+    public abstract T getLeft();
 
-    @SuppressWarnings("null")
+    public abstract S getRight();
+
     public static <T> T getLeftOrRight(Either<T, T> value) {
         if (value.isLeft()) {
             return value.getLeft();
@@ -66,3 +51,50 @@ public final class Either<T, S> {
     }
 }
 
+final class Left<T, S> extends Either<T, S> {
+
+    private final T value;
+
+    Left(T value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean isLeft() {
+        return true;
+    }
+
+    @Override
+    public T getLeft() {
+        return value;
+    }
+
+    @Override
+    public S getRight() {
+        throw new NoSuchElementException("No value present");
+    }
+}
+
+final class Right<T, S> extends Either<T, S> {
+
+    private final S value;
+
+    Right(S value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean isLeft() {
+        return false;
+    }
+
+    @Override
+    public T getLeft() {
+        throw new NoSuchElementException("No value present");
+    }
+
+    @Override
+    public S getRight() {
+        return value;
+    }
+}
