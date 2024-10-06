5a12671e6eeb8adc13f05e5f94770ec76aecf5af
Ronald van Manen <rvanmanen@gmail.com>
2024-10-06 07:30:52 +0200

    Replace StringParser tests with property-based tests.

--- src/test/java/com/ragnvaldr/alphaomega/parsing/StringParserTests.java
+++ src/test/java/com/ragnvaldr/alphaomega/parsing/StringParserTests.java
@@ -19,6 +19,7 @@
 // 3. This notice may not be removed or altered from any source distribution.
 package com.ragnvaldr.alphaomega.parsing;
 
+import java.util.List;
 import java.util.NoSuchElementException;
 
 import com.ragnvaldr.alphaomega.Scanner;
@@ -26,35 +27,70 @@ import com.ragnvaldr.alphaomega.Scanner;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import net.jqwik.api.Arbitraries;
+import net.jqwik.api.Arbitrary;
+import net.jqwik.api.ForAll;
+import net.jqwik.api.Property;
+import net.jqwik.api.Provide;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 final class StringParserTests {
 
-    @Test
-    void parseSucceeds() {
-        var scanner = new Scanner("Hello, World!");
+    final String source = "Hello, World!";
+
+    final List<String> matchingStrings = List.of(
+        "",
+        "H",
+        "He",
+        "Hel",
+        "Hell",
+        "Hello",
+        "Hello,",
+        "Hello, ",
+        "Hello, W",
+        "Hello, Wo",
+        "Hello, Wor",
+        "Hello, Worl",
+        "Hello, World",
+        "Hello, World!"
+    );
+
+    @Property
+    void parseSucceeds(@ForAll("parseSucceedsFor") String value) {
+        var scanner = new Scanner(source);
         var initialPosition = scanner.getPosition();
-        var parser = new StringParser("Hello");
+        var parser = new StringParser(value);
 
         var parseResult = parser.parse(scanner);
 
         assertTrue(parseResult.isSuccess());
-        assertEquals("Hello", parseResult.getValue());
-        assertEquals(5, scanner.getPosition() - initialPosition);
+        assertEquals(value, parseResult.getValue());
+        assertEquals(value.length(), scanner.getPosition() - initialPosition);
     }
 
-    @Test
-    void parseFails() {
-        var scanner = new Scanner("Hello, World!");
+    @Provide
+    Arbitrary<String> parseSucceedsFor() {
+        return Arbitraries.of(matchingStrings);
+    }
+
+    @Property
+    void parseFails(@ForAll("parseFailsFor") String value) {
+        var scanner = new Scanner(source);
         var initialPosition = scanner.getPosition();
-        var parser = new StringParser("World!");
+        var parser = new StringParser(value);
 
         var parseResult = parser.parse(scanner);
 
         assertTrue(parseResult.isFailure());
         assertThrows(NoSuchElementException.class, () -> parseResult.getValue());
         assertEquals(0, scanner.getPosition() - initialPosition);
+    }
+
+    @Provide
+    Arbitrary<String> parseFailsFor() {
+        return Arbitraries.strings().filter(z -> !matchingStrings.contains(z));
     }
 
     @Test
